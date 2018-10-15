e67d96ea2936d86e940eb89558ec3d4f12429345
Stephen Thompson <zenjabba@gmail.com>
2018-10-15 17:38:39 -0400

    fixed start process

--- spinup.sh
+++ spinup.sh
@@ -31,16 +31,14 @@ spin_up_instance () {
 
 
 gcloud beta compute instances create $INSTANCE_NAME --quiet --zone=$ZONE \
---machine-type=$MACHINE_TYPE --subnet=default --network-tier=PREMIUM --no-restart-on-failure \
+--machine-type=$MACHINE_TYPE --subnet=default --network-tier=STANDARD --no-restart-on-failure \
 --maintenance-policy=TERMINATE --preemptible  \
 --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
 --image=ubuntu-1604-xenial-v20181004 \
 --image-project=ubuntu-os-cloud \
 --boot-disk-size=10GB \
 --boot-disk-type=pd-standard \
---boot-disk-device-name=$INSTANCE_NAME \
---metadata startup-script='curl https://raw.githubusercontent.com/zenjabba/f1-control/master/install-gce-copier.sh | sudo bash'
-
+--boot-disk-device-name=$INSTANCE_NAME 
 }
 
 configure_rclone () {
@@ -50,8 +48,9 @@ echo "Configure rclone for your new instance $BOLD$INSTANCE_NAME $NORMAL"
 gcloud compute config-ssh --quiet > /dev/null
 gcloud compute ssh --zone $ZONE $INSTANCE_NAME -- 'mkdir -p /root/.config/rclone'
 echo "Please define source:/ for source location and destination:/ for destination location"
+gcloud compute ssh --zone --quiet $ZONE $INSTANCE_NAME -- 'curl https://raw.githubusercontent.com/zenjabba/f1-control/master/install-gce-copier.sh | sudo bash'
 gcloud compute ssh --zone $ZONE $INSTANCE_NAME -- '/usr/bin/rclone config --config=/root/.config/rclone/rclone.conf'
-gcloud compute ssh --zone $ZONE $INSTANCE_NAME -- 'reboot'
+gcloud compute ssh --zone --quiet $ZONE $INSTANCE_NAME -- 'reboot'
 
 }
 
