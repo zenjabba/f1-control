00fee4187d13140ea3442472a151ae62cd443413
Stephen Thompson <zenjabba@gmail.com>
2018-10-15 19:12:14 -0400

    need to be real root

--- spinup.sh
+++ spinup.sh
@@ -22,7 +22,7 @@ ZONE=$2
 
 get_default_project () {
 
-PROJECTID=$(gcloud projects list --uri)
+PROJECTID=$(sudo -s gcloud projects list --uri)
 PROJECTID=$(basename $PROJECTID)
 
 }
@@ -30,7 +30,7 @@ PROJECTID=$(basename $PROJECTID)
 spin_up_instance () {
 
 
-gcloud beta compute instances create $INSTANCE_NAME --quiet --zone=$ZONE \
+sudo -s gcloud beta compute instances create $INSTANCE_NAME --quiet --zone=$ZONE \
 --machine-type=$MACHINE_TYPE --subnet=default --network-tier=STANDARD --no-restart-on-failure \
 --maintenance-policy=TERMINATE --preemptible  \
 --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
@@ -45,19 +45,19 @@ configure_rclone () {
 
 echo "Configure rclone for your new instance $BOLD$INSTANCE_NAME $NORMAL"
 
-gcloud compute config-ssh --quiet > /dev/null
-gcloud compute ssh --zone $ZONE $INSTANCE_NAME -- 'mkdir -p /root/.config/rclone'
+sudo -s gcloud compute config-ssh --quiet > /dev/null
+sudo -s gcloud compute ssh --zone $ZONE $INSTANCE_NAME -- 'mkdir -p /root/.config/rclone'
 
-gcloud compute ssh  --quiet --zone $ZONE $INSTANCE_NAME -- 'curl https://raw.githubusercontent.com/zenjabba/f1-control/master/install-gce-copier.sh | sudo bash'
+sudo -s gcloud compute ssh  --quiet --zone $ZONE $INSTANCE_NAME -- 'curl https://raw.githubusercontent.com/zenjabba/f1-control/master/install-gce-copier.sh | sudo bash'
 echo "Please define source:/ for source location and destination:/ for destination location"
-gcloud compute ssh --zone $ZONE $INSTANCE_NAME -- '/usr/bin/rclone config --config=/root/.config/rclone/rclone.conf'
-gcloud compute ssh --quiet --zone $ZONE $INSTANCE_NAME -- 'reboot'
+sudo -s gcloud compute ssh --zone $ZONE $INSTANCE_NAME -- '/usr/bin/rclone config --config=/root/.config/rclone/rclone.conf'
+sudo -s gcloud compute ssh --quiet --zone $ZONE $INSTANCE_NAME -- 'reboot'
 
 }
 
 configure_gcloud () {
 
-gcloud config set project $PROJECTID
+sudo -s gcloud config set project $PROJECTID
 
 }
 
@@ -92,4 +92,4 @@ fi
 configure_rclone
 generate_crontab
 
-echo "Access this instance with the command $BOLD# gcloud beta compute ssh $INSTANCE_NAME --zone $ZONE --project=$PROJECTID$NORMAL"
+echo "Access this instance with the command $BOLD# sudo -s gcloud beta compute ssh $INSTANCE_NAME --zone $ZONE --project=$PROJECTID$NORMAL"
