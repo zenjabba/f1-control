00006f3bd5f20205e4e394cc917a285b5e08a64e
Stephen Thompson <zenjabba@gmail.com>
2018-12-10 11:08:06 -0500

    Include Billing Check

--- configure-gce.sh
+++ configure-gce.sh
@@ -2,6 +2,14 @@
 # First run to set things up correctly
 #
 
+gcloud_billing_check () {
+	billing=$(gcloud beta billing accounts list | grep "\<True\>")
+	if [ "$billing" == "" ]; then 
+	echo "Billing is not enabled on your account" 
+	exit 2
+}
+
+
 gcloud_auth () {
 
 sudo -s gcloud auth login --quiet $1
@@ -16,6 +24,7 @@ echo "/opt/f1-control/spinup.sh first-instance-name us-west2-a $1 to spin up you
 }
 
 gcloud_auth
+gcloud_billing_check
 spin_up_instance_first
 
 exit
